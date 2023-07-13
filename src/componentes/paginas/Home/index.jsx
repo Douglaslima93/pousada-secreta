99d2b3e395e40ff49c1018f0a3df55c6f1ae2721
Douglas Lima <douglas07a@gmail.com>
2023-07-13 12:47:51 -0300

    conteudo home

--- src/componentes/paginas/Home/index.jsx
+++ src/componentes/paginas/Home/index.jsx
@@ -9,13 +9,33 @@ const Home = () => {
         <EstruturaPagina>
             <main>
                 <Banner/>
-                <Acomodacoes>
-                <h2>Seja Bem Vindo(A)!</h2>
-                
-                </Acomodacoes>
+                <section className="container-bem-vindo">
 
-                
-                    
+                <div className="texto-bem-vindo">
+                <h2>Seja Bem-Vindo(A!)</h2>
+                <p>Relaxe em nossas acomodações e curta o melhor de Angra dos Reis.</p>
+                <p>Temos quartos para solteiro, casal ou familia.</p>
+                </div>
+
+                <div className="container-quartos">
+
+                   <div className="quartos">
+                    <img src="../img/quarto-solteiro1.jpg" alt="solteiro1"/>
+                    <strong>Quarto de Solteiro</strong>
+                    </div> 
+
+                    <div className="quartos">
+                        <img src="../img/quarto-casal1.jpg" alt="casal1"/>
+                        <strong>Quarto de Casal</strong>
+                    </div>
+
+                    <div className="quartos">
+                        <img src="../img/quarto-familia1.jpg" alt="familia1"/>
+                        <strong>Quarto de Familia</strong>
+                    </div>
+                </div>
+                </section> 
+
             </main>
         </EstruturaPagina>
     )
